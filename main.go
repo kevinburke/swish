40a63999f4c1586b9aa67a8f7d04ce49d8189f0f
Kevin Burke <kev@inburke.com>
2017-10-21 12:10:45 -0700

    Correct files

--- main.go
+++ main.go
@@ -1,24 +1,19 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"os"
-	"os/exec"
-	"os/signal"
 	"os/user"
 	"path/filepath"
-	"syscall"
 
 	"github.com/dchest/safefile"
 	"github.com/kevinburke/ssh_config"
 )
 
-func setHost(host string, identityFile string, username string) (error, string) {
-	user, err := user.Current()
-	if err != nil {
-		return err, "finding current user"
-	}
-	f, err := os.Open(filepath.Join(user.HomeDir, ".ssh", "config"))
+func setHost(configFile, host, identityFile, username string) (error, string) {
+	f, err := os.Open(configFile)
 	if err != nil {
 		return err, "opening ssh config file"
 	}
@@ -74,60 +69,18 @@ func checkError(err error, msg string) {
 }
 
 func main() {
-	if len(os.Args) <= 1 {
-		fmt.Fprintf(os.Stderr, "usage: swish child\n")
-		os.Exit(1)
+	identityFile := flag.String("identity-file", "", "Identity file")
+	userArg := flag.String("user", "", "SSH User")
+	host := flag.String("host", "github.com", "Host")
+	flag.Parse()
+	if *userArg == "" && *identityFile == "" {
+		checkError(errors.New("please provide a user or identity file"), "parsing command line arguments")
 	}
-	err, msg := setHost("github.com", "~/.ssh/otto-github", "kevinburkeotto")
+	u, err := user.Current()
+	if err != nil {
+		checkError(err, "finding current user")
+	}
+	configFile := filepath.Join(u.HomeDir, ".ssh", "config")
+	err, msg := setHost(configFile, *host, *identityFile, *userArg)
 	checkError(err, msg)
-	var args []string
-	if len(os.Args) > 2 {
-		args = os.Args[2:]
-	}
-	cmd := exec.Command(os.Args[1], args...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	c := make(chan os.Signal, 100)
-	signal.Notify(c)
-	if err := cmd.Start(); err != nil {
-		hostErr, msg := setHost("github.com", "~/.ssh/github_rsa", "kevinburke")
-		checkError(hostErr, msg)
-
-		fmt.Fprintf(os.Stderr, "Command %q exited on start: %v\n", os.Args[1], err)
-		os.Exit(1)
-	}
-	done := make(chan error, 1)
-	go func(ch chan<- error) {
-		ch <- cmd.Wait()
-	}(done)
-	for {
-		select {
-		case sig := <-c:
-			if err := cmd.Process.Signal(sig); err != nil {
-				fmt.Fprintf(os.Stderr, "could not send signal %q to subprocess: %v\n", err)
-			}
-			continue
-		case err := <-done:
-			hostErr, msg := setHost("github.com", "~/.ssh/github_rsa", "kevinburke")
-			checkError(hostErr, msg)
-			if err != nil {
-				if exiterr, ok := err.(*exec.ExitError); ok {
-					// The program has exited with an exit code != 0
-
-					// This works on both Unix and Windows. Although package
-					// syscall is generally platform dependent, WaitStatus is
-					// defined for both Unix and Windows and in both cases has
-					// an ExitStatus() method with the same signature.
-					if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
-						os.Exit(status.ExitStatus())
-					}
-				}
-				fmt.Fprintf(os.Stderr, "Command %q exited with unknown error: %v\n", os.Args[1], err)
-				os.Exit(1)
-			}
-			return
-		}
-	}
 }
